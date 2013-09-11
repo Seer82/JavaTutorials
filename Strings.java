0245129eb2d328420c84dacc861d958ba3cd7776
Seer82 <mp_cartwright@hotmail.com>
2013-09-11 15:37:41 +0100

    concatenating strings

---
+++ Strings.java
@@ -0,0 +1,27 @@
+public class Strings {
+
+	public static void main(String[] args) {
+
+		
+		String text = "hello";
+		// String is not a primitive data type but a class, in this case a type
+		// of object that can hold text
+
+		String empty = " ";
+		String identity = "humanoid";
+		String welcome = text + empty + identity;
+		// here is an example of concatenating strings
+		System.out.println(welcome);
+		
+		System.out.println("hello" + " " + "humanoid");
+		//different method of concatenation 
+		
+		int age = 55;
+		
+		System.out.println("my age is " + age);
+		// concatenating a strin
+		
+
+	}
+
+}
