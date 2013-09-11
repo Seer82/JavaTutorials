31090eaee4f0fc75e37c942c43e2650b63ffa67b
Seer82 <mp_cartwright@hotmail.com>
2013-09-11 15:01:48 +0100

    Create Variables.java

---
+++ Variables.java
@@ -0,0 +1,18 @@
+public class variable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int Num1;
+		//declaring a variable in this case a integer called Num1
+		
+		Num1= 53;
+		//Initialising the variable Num1 to the value of 53
+		// int Num1 = 53; is a quicker way of initialising the variable
+		
+		System.out.println(Num1);
+		//output the value of the variable Num1
+
+	}
+
+}
