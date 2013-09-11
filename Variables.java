a5a613dea11cc0d9824737929a6df234d60eba37
Seer82 <mp_cartwright@hotmail.com>
2013-09-11 15:10:52 +0100

    Update Variables.java

--- Variables.java
+++ Variables.java
@@ -1,10 +1,11 @@
+
 public class variable {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		int Num1;
-		//declaring a variable in this case an integer called Num1
+		//declaring a variable in this case a integer called Num1
 		
 		Num1= 53;
 		//Initialising the variable Num1 to the value of 53
@@ -12,7 +13,18 @@ public class variable {
 		
 		System.out.println(Num1);
 		//output the value of the variable Num1
+		
+		
+		short short1 = 652;
+		long long1 = 736276;
+		double double1 = 6.356376;
+		float float1 = 367.7f;
+		
+		//variables of different datatypes
+		
+		System.out.println(short1);
+		System.out.println(long1);
+		System.out.println(double1);
+		System.out.println(float1);
 
 	}
-
-}
