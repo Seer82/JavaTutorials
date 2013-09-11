806f9dcbe3c8c3c8cca65ffd948f6acccb240f22
Seer82 <mp_cartwright@hotmail.com>
2013-09-11 16:56:46 +0100

    Create ForLoops.java
    For Loops conditions and arguments

---
+++ ForLoops.java
@@ -0,0 +1,20 @@
+public class ForLoops {
+
+	public static void main(String[] args) {
+
+		for (int i = 0; i < 5; i++) {
+			// initial state of the variable
+			// loop conditional argument in this case if i is less than 5
+			// continue to loop
+			// incrementally increase i by one every time the loop is executed.
+			// stop loop before i reaches 5
+			
+			// print hello every time the loop is executed.
+			
+			System.out.printf("the value of i is: %d ", i);
+			//format specifier %d specifies a decimal value prints all on one line.
+			//%d\n to format the output on a new line
+		}
+
+	}
+}
